dcd3d8e98117ecbca58c31b100e68deb5ace92ca
Eric Guyader <ericguyader@yahoo.fr>
2018-05-28 15:40:09 +0200

    Initialise le projet

---
+++ js/todos.js
@@ -0,0 +1,42 @@
+const ToDos = [
+    {
+        id:1,
+        titre:'Faire la vaisselle',
+        description:'',
+        importance:1,
+        echeance:'2018-05-29',
+        finie: false
+    },
+    {
+        id:2,
+        titre:'Faire la danse du soleil',
+        description:'Surtout ne pas chanter, sous peine de faire revenir la pluie...',
+        importance:2,
+        echeance:'2018-05-30',
+        finie: false
+    },
+    {
+        id:3,
+        titre:'Signer la déclaration d\'impôts',
+        description:'Sur le site Web des impôts',
+        importance:3,
+        echeance:'2018-05-29',
+        finie: false
+    },
+    {
+        id:4,
+        titre:'Apprendre Vue.js',
+        description:'',
+        importance:3,
+        echeance:'2018-06-01',
+        finie: false
+    },
+    {
+        id:5,
+        titre:'Courir 5km',
+        description:'Si possible sans s\'arrêter et en moins de 30mn',
+        importance:2,
+        echeance:'2018-05-30',
+        finie: false
+    }
+];
