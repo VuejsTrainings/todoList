1a6c37889c7f95c74bc23cc772ec6f4e08c77d97
Eric Guyader <ericguyader@yahoo.fr>
2018-05-29 11:07:51 +0200

    Ajoute filtre upperCase sur application et l'utilise sur le nom utilisateur

--- js/app.js
+++ js/app.js
@@ -1,9 +1,18 @@
 new Vue({
     el:'#app',
     data:  {
-        user: 'Eric'
+        user: 'eric'
     },
     components:{
         'todo-list': ToDosListComponent
+    },
+    filters:{
+        /**
+         * Convertit en majuscules
+         * @param {String} val 
+         */
+        toUpper(val){
+            return val.toUpperCase();
+        }
     }
 })
